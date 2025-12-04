8c01a72a5abd9484183298752056a8d50c083211
Yashaswini <yashaswinim2018@email.com>
2025-12-04 20:44:21 +0000

    took server code from anushka file

--- shiny/app.R
+++ shiny/app.R
@@ -163,8 +163,157 @@ ui <- dashboardPage(
     )
   )
 )
-server <- function(input, output, session) 
-{
+
+  server <- function(input, output, session) {
+    
+    filtered <- reactive({
+      req(input$age_range, input$sex_filter, input$trt_filter)
+      dig %>%
+        filter(
+          AGE >= input$age_range[1],
+          AGE <= input$age_range[2],
+          SEX %in% input$sex_filter,
+          TRTMT %in% input$trt_filter
+        )
+    })
+    
+    output$n_patients <- renderValueBox({
+      valueBox(
+        value = nrow(dig),
+        subtitle = "Patients",
+        icon = icon("users"),
+        color = "purple"
+      )
+    })
+    
+    output$pct_treat <- renderValueBox({
+      pct <- mean(dig$TRTMT == "Digoxin") * 100
+      valueBox(
+        value = sprintf("%.1f%%", pct),
+        subtitle = "On digoxin",
+        icon = icon("capsules"),
+        color = "green"
+      )
+    })
+    
+    output$mean_age <- renderValueBox({
+      valueBox(
+        value = sprintf("%.1f", mean(dig$AGE, na.rm = TRUE)),
+        subtitle = "Mean age (years)",
+        icon = icon("user-clock"),
+        color = "yellow"
+      )
+    })
+    output$age_hist <- renderPlot({
+      ggplot(dig, aes(x = AGE)) +
+        geom_histogram(bins = 30) +
+        labs(x = "Age (years)", y = "Count")
+    })
+    
+    output$ef_hist <- renderPlot({
+      ggplot(dig, aes(x = EJF_PER)) +
+        geom_histogram(bins = 30) +
+        labs(x = "Ejection fraction (%)", y = "Count")
+    })
+    
+    baseline_data <- reactive({
+      d <- dig
+      if (input$baseline_trt != "Both") {
+        d <- d %>% filter(TRTMT == input$baseline_trt)
+      }
+      if (input$baseline_sex != "Both") {
+        d <- d %>% filter(SEX == input$baseline_sex)
+      }
+      d
+    })
+    
+    output$baseline_cont_plot <- renderPlot({
+      var <- input$baseline_cont
+      ggplot(baseline_data(), aes(x = TRTMT, y = .data[[var]])) +
+        geom_boxplot() +
+        labs(x = "Treatment group", y = names(cont_vars)[cont_vars == var])
+    })
+    
+    output$baseline_cont_table <- renderDataTable({
+      var <- input$baseline_cont
+      baseline_data() %>%
+        group_by(TRTMT) %>%
+        summarise(
+          n = n(),
+          mean = mean(.data[[var]], na.rm = TRUE),
+          sd = sd(.data[[var]], na.rm = TRUE)
+        )
+    })
+    
+    output$baseline_cat_plot <- renderPlot({
+      var <- input$baseline_cat
+      ggplot(baseline_data(), aes(x = .data[[var]], fill = TRTMT)) +
+        geom_bar(position = "fill") +
+        scale_y_continuous(labels = scales::percent_format()) +
+        labs(x = names(cat_vars)[cat_vars == var], y = "Proportion")
+    })
+    
+    output$baseline_cat_table <- renderDataTable({
+      var <- input$baseline_cat
+      baseline_data() %>%
+        count(TRTMT, !!sym(var)) %>%
+        group_by(TRTMT) %>%
+        mutate(pct = n / sum(n))
+    })
+    
+    outcome_data <- reactive({
+      dig %>%
+        filter(!is.na(.data[[input$outcome_var]]))
+    })
+    
+    output$outcome_plot <- renderPlot({
+      ggplot(outcome_data(),
+             aes(x = .data[[input$outcome_group]],
+                 fill = .data[[input$outcome_var]])) +
+        geom_bar(position = "fill") +
+        scale_y_continuous(labels = scales::percent_format()) +
+        labs(x = input$outcome_group,
+             y = "Proportion",
+             fill = names(outcome_vars)[outcome_vars == input$outcome_var])
+    })
+    
+    output$outcome_table <- renderDataTable({
+      outcome_data() %>%
+        count(.data[[input$outcome_group]], .data[[input$outcome_var]]) %>%
+        group_by(.data[[input$outcome_group]]) %>%
+        mutate(pct = n / sum(n))
+    })
+    
+    output$rel_plot <- renderPlot({
+      xvar <- input$xvar
+      yvar <- input$yvar
+      colour_var <- input$rel_colour
+      
+      p <- ggplot(dig, aes(x = .data[[xvar]], y = .data[[yvar]]))
+      
+      if (colour_var != "None") {
+        p <- p + aes(colour = .data[[colour_var]])
+      }
+      
+      p <- p + geom_point(alpha = 0.5)
+      
+      if (input$add_smooth) {
+        p <- p + geom_smooth(method = "loess", se = FALSE)
+      }
+      
+      p + labs(
+        x = names(cont_vars)[cont_vars == xvar],
+        y = names(cont_vars)[cont_vars == yvar]
+      )
+    })
+    
+    output$data_table <- renderDT({
+      datatable(dig, options = list(pageLength = 25))
+    })
+  }
   
-}
+  
+  
+  shinyApp(ui = ui, server = server)
+
 shinyApp(ui, server)
